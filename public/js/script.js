70ace0c997edf5e68cbe3aac7526bc9cf0a6b7b2
Jimmy Lin <usmcjimmy@gmail.com>
2016-06-24 17:47:18 -0400

    functioning mobile wechat add with swipe scroll and arrow down

--- public/js/script.js
+++ public/js/script.js
@@ -2,7 +2,9 @@ $(function(){
   console.log("running script file")
 
   // fullpage.js
-  $('#fullpage').fullpage();
+  $('#fullpage').fullpage({
+    anchors:['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'fivePage']
+  });
 
 
 
