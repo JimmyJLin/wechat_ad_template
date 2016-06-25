4feb74fe8b586048ac5f9488bf4cc4886391c7d3
Jimmy Lin <usmcjimmy@gmail.com>
2016-06-25 07:48:32 -0400

    functioning mobile playing audio on clickevent

--- public/js/script.js
+++ public/js/script.js
@@ -6,14 +6,15 @@ $(function(){
     anchors:['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'fivePage']
   });
 
+  // mobile autoplay audio on clickevent
   function play(){
     const audio = document.getElementById('audio')
     audio.play()
   }
-
   $('#pageOnearrowdown').on('click', function(){
     play()
   })
+
 
 
 
